f2001fcfc9b43b8599ff7e49a94c91e8eb68b5fc
Cheshire <jaeho406@gmail.com>
2017-12-14 23:55:52 +0900

    Use Mongoose
    Add Index, Concrete Modeling

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,11 +2,44 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName : String,
-    lastName : String,
-    email : String,
-    username : String,
-    password : String
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        index: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true
+    },
+    password: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        set: function (url) {
+            if(!url) {
+                return url;
+            } else {
+                if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+
+                return url;
+            }
+        }
+    }
+});
+
+UserSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+}).set(function (fullName) {
+    var spiltName = fullName.spilt(' ');
+    this.firstName = spiltName[0] || '';
+    this.lastName = spiltName[1] || '';
 });
 
 mongoose.model('User', UserSchema);
