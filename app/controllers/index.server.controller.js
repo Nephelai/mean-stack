8452ab11e34ce539ffc84da08afd3b2bb91b547f
Cheshire <jaeho406@gmail.com>
2017-12-18 21:25:24 +0900

    Use AngularJS Service
    This is angularJS service 구현

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -7,6 +7,6 @@ exports.render = function (req, res) {
 
     res.render('index', {
         title: 'Hello EJS!',
-        userFullName: req.user ? req.user.fullName: ''
+        user: JSON.stringify(req.user)
     });
 };
--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,3 +1,4 @@
-angular.module('example').controller('ExampleController', ['$scope', function ($scope) {
-    $scope.name = 'MEAN Apllication';
+angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
+    function ($scope, Authentication) {
+    $scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
 }]);
