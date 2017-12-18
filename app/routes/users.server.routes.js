075bc9cbdedb455bd87f18dbe9d6e4570479ea14
Cheshire <jaeho406@gmail.com>
2017-12-18 17:01:38 +0900

    Success with passport
    use facebook, google.
    it is possisble with login.

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -16,5 +16,27 @@ module.exports = function (app) {
             failureFlash: true
         }));
 
+    app.get('/oauth/facebook', passport.authenticate('facebook', {
+        failureRedirect: '/signin'
+    }));
+
+    app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
+
+    app.get('/oauth/google', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ],
+    }));
+
+    app.get('/oauth/google/callback', passport.authenticate('google', {
+        failureRedirect: '/signin',
+        successRedirect: '/'
+    }));
+
     app.get('/signout', users.signout);
 };
--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,15 @@ module.exports = {
     // development composition option
     db: 'mongodb://localhost/mean-book',
     // general mongoDB URI - mongodb://username:password@hostname:port/database
-    sessionSecret: 'cheshireSessionSecret'
+    sessionSecret: 'cheshireSessionSecret',
+    facebook: {
+        clientID: '144013946203800',
+        clientSecret: '7163da4b99e60f081d401246c06b1a0c',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    google: {
+        clientID: '205310833239-o8k7gms0nuu16s5vg4thvng19ii34nmu.apps.googleusercontent.com',
+        clientSecret: 'luC_plW1VkDviCDS-NHOAf59',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
+    }
 };
