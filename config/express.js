cc36a8b18e7c6d6352e62e892f3eaed6690fa2c5
Cheshire <jaeho406@gmail.com>
2017-12-24 14:31:00 +0900

    socketio server & session
    socketio server & session process

--- config/express.js
+++ config/express.js
@@ -1,16 +1,21 @@
 var config = require('./config'),
+    http = require('http'),
+    socketio = require('socket.io'),
     express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
     flash = require('connect-flash'),
     passport = require('passport');
 
 
 module.exports = function () {
     var app = express();
+    var server = http.createServer(app);
+    var io = socketio.listen(server);
 
     if (process.env.NODE_ENV === 'development') {
         app.use(morgan('dev'));
@@ -24,10 +29,15 @@ module.exports = function () {
     app.use(bodyParser.json());
     app.use(methodOverride());
 
+    var mongoStore = new MongoStore({
+        db: db.connection.db
+    });
+
     app.use(session({
-            saveUninitialized: true,
+        saveUninitialized: true,
         resave: true,
-        secret: config.sessionSecret
+        secret: config.sessionSecret,
+        store: mongoStore
     }));
 
     app.set('views', './app/views');
@@ -43,5 +53,5 @@ module.exports = function () {
 
     app.use(express.static('./public'));
 
-    return app;
+    return server;
 };
